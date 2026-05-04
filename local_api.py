60617747f2ea50d1ed27009405e1120627c4845b
kwashco <kwashco@wgu.edu>
2026-05-04 06:02:15 +0000

    Fix flake8 issues across project

--- local_api.py
+++ local_api.py
@@ -1,15 +1,12 @@
-import json
-
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+r = requests.get("http://127.0.0.1:8000")
 
+
+print("Status code:", r.status_code)
+
+print("Result:", r.json()["message"])
 
 
 data = {
@@ -29,10 +26,10 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = None # Your code here
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+r = requests.post("http://127.0.0.1:8000/data/", json=data)
+
+
+print("Status Code:", r.status_code)
+
+print("Result:", r.json()["result"])
